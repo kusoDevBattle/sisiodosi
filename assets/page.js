58c9093f6fbe04837398e2be6c6c917bb2920e74
Sota NAKAGAMI <athene609.cyl@gmail.com>
2017-03-10 02:27:00 +0900

    ファーストクソ放流
    ### 機能
    - 鹿威しクリックorスペースキーで鹿威す
    - 威しカウント
    - BGM再生（サウンドをクリックor"b"）
    - BGM再生可否をクッキーで保存（要検討）
    
    ### 将来的な実装予定（希望も含む）
    - 威し数をサーバー管理して共有
    - 誰かが威すと全部鳴る
    - BGM種類増
    - 威し時のアニメーション

---
+++ assets/page.js
@@ -0,0 +1,83 @@
+"use strict";
+document.addEventListener('contextmenu', function () { return event.preventDefault(); });
+document.addEventListener('DOMContentLoaded', function () { return new Application(); });
+var Application = (function () {
+    function Application() {
+        var _this = this;
+        this.cookie = new Cookie();
+        this.counter = 0;
+        this.bgm = document.getElementById('ElmBgm');
+        this.se = document.getElementById('ElmSe');
+        this.setBgm(this.cookie.getAttribute('bgm') === 'true');
+        document.getElementById('S7i').addEventListener('click', (function () { return _this.click(); }));
+        document.getElementById('Bgm').addEventListener('click', (function () { return _this.toggleBgm(); }));
+        window.addEventListener('keydown', (function (e) { return _this.keyEvents(e); }));
+    }
+    Application.prototype.click = function () {
+        this.counter++;
+        document.getElementById('Count').innerText = this.counter.toString();
+        this.se.currentTime = 0;
+        this.se.play();
+    };
+    Application.prototype.keyEvents = function (e) {
+        e.preventDefault();
+        if (e.key === ' ')
+            this.click();
+        if (e.key === 'b')
+            this.toggleBgm();
+    };
+    Application.prototype.toggleBgm = function () {
+        this.setBgm(this.bgm.paused || this.bgm.ended);
+    };
+    Application.prototype.setBgm = function (doPlay) {
+        this.cookie.setAttribute('bgm', doPlay);
+        if (doPlay) {
+            this.bgm.play();
+        }
+        else {
+            this.bgm.pause();
+        }
+    };
+    return Application;
+}());
+var Cookie = (function () {
+    function Cookie() {
+        this.attrs = {};
+        this.init();
+        this.pullAttributes();
+        var v = this.getAttribute('bgm') || 'false';
+        this.setAttribute('bgm', v);
+    }
+    Cookie.prototype.getAttribute = function (key) {
+        this.pullAttributes();
+        return (this.attrs[key]) ? this.attrs[key] : null;
+    };
+    Cookie.prototype.getAttributes = function () {
+        this.pullAttributes();
+        return this.attrs;
+    };
+    Cookie.prototype.init = function () {
+        this.setAttribute('sisiodosi', 'wabisabi');
+        this.removeAttribute('');
+    };
+    Cookie.prototype.pullAttributes = function () {
+        var cookies = document.cookie.split(';');
+        for (var i in cookies) {
+            var cookie = cookies[i].split('=');
+            this.attrs[cookie[0]] = cookie[1];
+        }
+    };
+    Cookie.prototype.removeAttribute = function (key) {
+        var cookies = '';
+        for (var i in this.attrs) {
+            if (i !== key)
+                cookies += i + "=" + this.attrs[i] + ";";
+        }
+        document.cookie = cookies;
+    };
+    Cookie.prototype.setAttribute = function (key, val) {
+        this.attrs[key] = val;
+        document.cookie = key + '=' + val + ';';
+    };
+    return Cookie;
+}());
---
+++ src/page.ts
@@ -0,0 +1,90 @@
+document.addEventListener('contextmenu', ()=>event!.preventDefault())
+document.addEventListener('DOMContentLoaded', ()=>new Application())
+
+
+
+
+
+class Application {
+  private cookie :Cookie
+  private counter :number
+  private bgm :HTMLAudioElement
+  private se  :HTMLAudioElement
+  constructor() {
+    this.cookie = new Cookie()
+    this.counter = 0
+    this.bgm = document.getElementById('ElmBgm')! as HTMLAudioElement
+    this.se  = document.getElementById('ElmSe')!  as HTMLAudioElement
+    this.setBgm(this.cookie.getAttribute('bgm')==='true')
+    document.getElementById('S7i')!.addEventListener('click', (()=>this.click()))
+    document.getElementById('Bgm')!.addEventListener('click', (()=>this.toggleBgm()))
+    window.addEventListener('keydown', ((e:any)=>this.keyEvents(e)))
+  }
+  private click() {
+    this.counter++
+    document.getElementById('Count')!.innerText = this.counter.toString()
+    this.se.currentTime = 0
+    this.se.play()
+  }
+  private keyEvents(e :KeyboardEvent) {
+    e.preventDefault()
+    if(e.key === ' ') this.click()
+    if(e.key === 'b') this.toggleBgm()
+  }
+  private toggleBgm() {
+    this.setBgm(this.bgm.paused || this.bgm.ended)
+  }
+  private setBgm(doPlay:boolean) {
+    this.cookie.setAttribute('bgm', doPlay)
+    if(doPlay) {
+      this.bgm.play()
+    }else{
+      this.bgm.pause()
+    }
+  }
+}
+
+
+
+
+
+class Cookie {
+  private attrs :any
+  constructor() {
+    this.attrs = {}
+    this.init()
+    this.pullAttributes()
+    let v = this.getAttribute('bgm') || 'false'
+    this.setAttribute('bgm', v)
+  }
+  getAttribute(key:string) :string|null {
+    this.pullAttributes()
+    return (this.attrs[key])? this.attrs[key] : null
+  }
+  getAttributes() {
+    this.pullAttributes()
+    return this.attrs
+  }
+  private init() {
+    this.setAttribute('sisiodosi', 'wabisabi')
+    this.removeAttribute('')
+  }
+  pullAttributes() {
+    let cookies = document.cookie.split(';')
+    for(let i in cookies) {
+      let cookie = cookies[i].split('=')
+      this.attrs[cookie[0]] = cookie[1]
+    }
+  }
+  removeAttribute(key:string) {
+    let cookies = ''
+    for(let i in this.attrs) {
+      if(i!==key) cookies += `${i}=${this.attrs[i]};`
+    }
+    document.cookie = cookies
+  }
+  setAttribute(key:string, val:any) {
+    this.attrs[key] = val
+    document.cookie = key + '=' + val + ';'
+  }
+}
